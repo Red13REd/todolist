e22a45308038fc3e0212b199b2e369e2aaeb40f5
Red <amekysw@gmail.com>
2022-06-26 20:45:22 +0300

    New

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
